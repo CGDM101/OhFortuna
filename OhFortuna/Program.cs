2017254a40eec21f8f920440510a93ab7e0ed40b
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-09-17 21:50:12 +0200

    Refactoring

--- OhFortuna/Program.cs
+++ OhFortuna/Program.cs
@@ -9,20 +9,20 @@ namespace OhFortuna
         {
             int pix = 500;
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("*** VÄLKOMMEN ***");
-            Thread.Sleep(1000);
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("*** VÄLKOMMEN ***");
-            Thread.Sleep(1000);
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("*** VÄLKOMMEN ***");
-            Thread.Sleep(1000);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write('.');Thread.Sleep(500); Console.Write('.'); Thread.Sleep(500); Console.Write('.'); Thread.Sleep(500);
-            Console.WriteLine();
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("*** VÄLKOMMEN ***");
+            //Thread.Sleep(1000);
+            //Console.Clear();
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine("*** VÄLKOMMEN ***");
+            //Thread.Sleep(1000);
+            //Console.Clear();
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("*** VÄLKOMMEN ***");
+            //Thread.Sleep(1000);
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.Write('.');Thread.Sleep(500); Console.Write('.'); Thread.Sleep(500); Console.Write('.'); Thread.Sleep(500);
+            //Console.WriteLine();
 
             bool done = false;
             while (!done && pix > 0)
@@ -34,72 +34,69 @@ namespace OhFortuna
                     Console.WriteLine("Välkommen åter.");
                     done = true;
                 }
-                else if(answer == "y" && pix > 50)
+                else if (answer == "y" && pix > 50)
                 {
-                    //int pix = 500;
-
                     Console.WriteLine($"Du har {pix} pix, satsa minst 50!");
                     int userSatsning = int.Parse(Console.ReadLine());
-                    pix -= userSatsning;
-
-                    Console.WriteLine("Du satsade: " + userSatsning);
-                    Console.WriteLine("Välj ett lyckotal mellan 1 och 6!");
-                    int userChoice = int.Parse(Console.ReadLine());
-                    if (userChoice >= 1 && userChoice <= 6) // while
+                    while (userSatsning > pix || userSatsning < 50) // går till rad 30 om satsar exakt 500 (el motsv)
                     {
-                        Console.WriteLine("Du valde: " + userChoice);
-
-                        Random dice1 = new Random();
-                        int dice1number = dice1.Next(1, 7);
-
-                        Random dice2 = new Random();
-                        int dice2number = dice1.Next(1, 7);
-
-                        Random dice3 = new Random();
-                        int dice3number = dice1.Next(1, 7);
-                        Console.WriteLine($"Tärningarna visar {dice1number}, {dice2number} och {dice3number}");
-
-                        if (userChoice != dice1number && userChoice != dice2number && userChoice != dice3number)
-                        {
-                            Console.WriteLine("Ingen tärning visade ditt lyckotal :(");
-                            Console.WriteLine("Du har: " + pix);
-                        }
-                        else if (userChoice == dice1number || userChoice == dice2number || userChoice == dice3number)
-                        {
-                            // visas även om två tärningar rätt:
-                            Console.WriteLine("En tärning visade rätt. Du får dubbla insatsen, dvs: " + userSatsning * 2);
-                            pix += (userSatsning * 2);
-                            Console.WriteLine("Det du har nu är: " + pix);
-                        }
-                        else if (userChoice == dice1number && userChoice == dice2number || userChoice == dice1number && userChoice == dice3number || userChoice == dice2number && userChoice == dice1number || userChoice == dice2number && userChoice == dice3number || userChoice == dice3number && userChoice == dice1number || userChoice == dice3number && userChoice == dice2number) // OBS
-                        {
-                            Console.WriteLine("Två tärningar visade rätt, du får tre gånger insatsen, dvs: " + userSatsning * 3);
-                            pix += (userSatsning * 3);
-                            Console.WriteLine("Det du har nu är: " + pix);
-
-                        }
-                        else if (userChoice == dice1number && userChoice == dice2number && userChoice == dice3number)
-                        {
-                            Console.WriteLine("Alla tärningar visade rätt! Du får fyra gånger insatsen, dvs: " + userSatsning * 4);
-                            pix += (userSatsning * 4);
-                            Console.WriteLine("Det du har nu är: " + pix);
-                        }
+                        Console.WriteLine("Du kan inte satsa mer än du har eller under 50! Satsa igen!");
+                        userSatsning = int.Parse(Console.ReadLine());
                     }
-                    else if(userChoice !>= 1 || userChoice !<= 6)
+                    if (userSatsning < pix)
                     {
-                        Console.WriteLine("Välj något mellan 1 och 6!"); // funkar bara en gång
-                        //done = true;
-                        userChoice = int.Parse(Console.ReadLine());
+                        pix -= userSatsning;
+                        Console.WriteLine("Du satsade: " + userSatsning);
 
-                        
+                        Console.WriteLine("Välj ett lyckotal mellan 1 och 6!");
+                        int userChoice = int.Parse(Console.ReadLine());
+                        while (userChoice < 1 || userChoice > 6) // mindre än ett ELLER större än 6
+                        {
+                            Console.WriteLine("Välj något mellan 1 och 6!");
+                            userChoice = int.Parse(Console.ReadLine());
+                        }
+                        if (userChoice >= 1 && userChoice <= 6) // större än ett OCH mindre än sex
+                        {
+                            Console.WriteLine("Du valde: " + userChoice);
+
+                            Random dice1 = new Random();
+                            int dice1number = dice1.Next(1, 7);
+
+                            Random dice2 = new Random();
+                            int dice2number = dice1.Next(1, 7);
+
+                            Random dice3 = new Random();
+                            int dice3number = dice1.Next(1, 7);
+                            Console.WriteLine($"Tärningarna visar {dice1number}, {dice2number} och {dice3number}");
+
+                            if (userChoice != dice1number && userChoice != dice2number && userChoice != dice3number)
+                            {
+                                Console.WriteLine("Ingen tärning visade ditt lyckotal :(");
+                                Console.WriteLine("Du har: " + pix);
+                            }
+                            else if (userChoice == dice1number || userChoice == dice2number || userChoice == dice3number)
+                            {
+                                // visas även om två tärningar rätt:
+                                Console.WriteLine("En tärning visade rätt. Du får dubbla insatsen, dvs: " + userSatsning * 2);
+                                pix += (userSatsning * 2);
+                                Console.WriteLine("Det du har nu är: " + pix);
+                            }
+                            else if (userChoice == dice1number && userChoice == dice2number || userChoice == dice1number && userChoice == dice3number || userChoice == dice2number && userChoice == dice1number || userChoice == dice2number && userChoice == dice3number || userChoice == dice3number && userChoice == dice1number || userChoice == dice3number && userChoice == dice2number) // OBS
+                            {
+                                Console.WriteLine("Två tärningar visade rätt, du får tre gånger insatsen, dvs: " + userSatsning * 3);
+                                pix += (userSatsning * 3);
+                                Console.WriteLine("Det du har nu är: " + pix);
+
+                            }
+                            else if (userChoice == dice1number && userChoice == dice2number && userChoice == dice3number)
+                            {
+                                Console.WriteLine("Alla tärningar visade rätt! Du får fyra gånger insatsen, dvs: " + userSatsning * 4);
+                                pix += (userSatsning * 4);
+                                Console.WriteLine("Det du har nu är: " + pix);
+                            }
+                        }
+
                     }
-
-
-
-
-
-
-
                 }
             }
         }
